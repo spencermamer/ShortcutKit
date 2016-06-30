de75ed5a84055484ea54c295941034b37a551aeb
Spencer B. Mamer <spencermamer@gmail.com>
2016-06-29 22:52:46 -0500

    Bam

---
+++ ShortcutKit iOS/ShortcutKit iOS.h
@@ -0,0 +1,19 @@
+//
+//  ShortcutKit iOS.h
+//  ShortcutKit iOS
+//
+//  Created by Spencer Mamer on 6/29/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ShortcutKit iOS.
+FOUNDATION_EXPORT double ShortcutKit_iOSVersionNumber;
+
+//! Project version string for ShortcutKit iOS.
+FOUNDATION_EXPORT const unsigned char ShortcutKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShortcutKit_iOS/PublicHeader.h>
+
+
